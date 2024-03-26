6650aa8d1bcbf3d15a9a47a15ea2df79ca2ee563
leandrogrng <leandro.garing@gmail.com>
2024-03-26 12:27:19 +0800

    Installed-packages

--- frontend-workshop/src/components/SideNav/routes.jsx
+++ frontend-workshop/src/components/SideNav/routes.jsx
@@ -1,13 +1,13 @@
 const routes = [
-  { path: "/", name: "Dashboard" },
-  { path: "/pHLevel", name: "pH Level" },
-  { path: "/water_temperature", name: "Water Temperature" },
-  { path: "/ec", name: "Electrical Conductivity" },
-  { path: "/tds", name: "Total Dissolved Solids" },
-  { path: "/moisture", name: "Cocopeat Moisture" },
-  { path: "/ambient_light", name: "Ambient Light" },
-  { path: "/temperature_humidity", name: "Temperature & Humidity" },
-  { path: "/ambient_light", name: "Ambient Light" },
+    { path: "/", name: "Dashboard" },
+    { path: "/pHLevel", name: "pH Level" },
+    { path: "/water_temperature", name: "Water Temperature" },
+    { path: "/ec", name: "Electrical Conductivity" },
+    { path: "/tds", name: "Total Dissolved Solids" },
+    { path: "/moisture", name: "Cocopeat Moisture" },
+    { path: "/ambient_light", name: "Ambient Light" },
+    { path: "/temperature_humidity", name: "Temperature & Humidity" },
+    { path: "/ambient_light", name: "Ambient Light" },
 ];
 
 export default routes;
