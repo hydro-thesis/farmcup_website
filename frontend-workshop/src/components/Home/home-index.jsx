01238f2a7f2211ea37937767e2dc6fa795a1915c
aeron-cap <aeroncaponpon.01@gmail.com>
2024-03-29 21:36:45 +0800

    Revert "Merge remote-tracking branch 'origin/create-dashboard-home' into pH-level-dashboard"
    This reverts commit d2903d1527c9e77a834694fc2c3d3a58e9612266, reversing
    changes made to 9b26a01c05c3e36d806a93d53efdf62ca4ab47df.

--- frontend-workshop/src/components/Home/home-index.jsx
+++ frontend-workshop/src/components/Home/home-index.jsx
@@ -1,33 +1,12 @@
-import React from 'react';
-import { Box, Flex, GridItem, Heading, SimpleGrid, Stat, StatArrow, StatHelpText, StatLabel, StatNumber } from '@chakra-ui/react';
-import data from '../../utils/parameters.json';
+import React from 'react'
 
-
-const HomeIndex = () => {
-    // Get the last item from the data array
-    const lastItem = data[data.length - 1];
-
+function HomeIndex() {
     return (
-        <Flex justify={'center'}>
-            
-            <SimpleGrid columns={{sm: 1, md: 3}} spacing={7} width={'100%'} pl = {75}>
-                {Object.keys(lastItem).map((key) => (
-                    <Stat as={GridItem} key={key} border={'1px'} borderRadius={'10px'} shadow={1}>
-                        <Box>
-                            <StatLabel as="h1" size="md" justifyContent={'left'}>
-                                {key}
-                            </StatLabel>
-                            <StatNumber>{lastItem[key]}</StatNumber>
-                            <StatHelpText>
-                                <StatArrow type='increase'/>
-                                20%
-                            </StatHelpText>
-                        </Box>
-                    </Stat>
-                ))}
-            </SimpleGrid>
-            </Flex>
-    );
-};
+        <div>
+            <h1 color='black'>Farm Cup Parameter Dashboard</h1>
+        
+        </div>
+    )
+}
 
 export default HomeIndex;
