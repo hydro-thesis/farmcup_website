316362d4a1b3d8b60cf534c8f67d0e2eb37da141
leandrogrng <leandro.garing@gmail.com>
2024-03-30 19:21:59 +0800

    added-units-file

---
+++ frontend-workshop/src/components/Home/display-units.jsx
@@ -0,0 +1,12 @@
+const unitMap = {
+    ph_level: '',
+    water_temperature: '°C',
+    water_ec: 'µS/cm',
+    tds: 'ppm',
+    water_level: '%',
+    moisture: '%',
+    ambient_light: 'lux',
+    ambient_temperature_humidity: '%',
+};
+
+export default unitMap;
