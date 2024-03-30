d6705788c3822b125ec8a78bb724c8e24eb9ee47
leandrogrng <leandro.garing@gmail.com>
2024-03-30 16:32:53 +0800

    created-key-display-for-parameters

---
+++ frontend-workshop/src/components/Home/key-display-names.jsx
@@ -0,0 +1,12 @@
+const keyDisplay = {
+    ph_level: 'pH Level of Water',
+    water_temperature: 'Water Temperature',
+    water_ec: 'Water Electrical Conductivity',
+    tds: 'Total Dissolved Solids (TDS)',
+    water_level: 'Water Level',
+    moisture: 'Cocopeat Moisture',
+    ambient_light: 'Ambient Light',
+    ambient_temperature_humidity: 'Ambient Temperature & Humidity'
+};
+
+export default keyDisplay;
