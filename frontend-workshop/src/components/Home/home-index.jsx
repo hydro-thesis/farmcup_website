810cdf421b391e72cc3516776e33d3b2fa8f272e
leandrogrng <leandro.garing@gmail.com>
2024-04-05 08:35:29 +0800

    display-units-per-parameter

--- frontend-workshop/src/components/Home/home-index.jsx
+++ frontend-workshop/src/components/Home/home-index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box, Flex, GridItem, SimpleGrid, Stat, StatArrow, StatHelpText, StatLabel, StatNumber } from '@chakra-ui/react';
 import data from '../../utils/parameters.json';
 import keyDisplay from './key-display-names';
+import unitMap from './display-units';
 
 const HomeIndex = () => {
     //Last item in the 'database' for updated values
@@ -14,14 +15,14 @@ const HomeIndex = () => {
 
     return (
         <Flex justify={'center'}>
-            <SimpleGrid columns={{sm: 1, md: 3}} spacing={7} width={'100%'} pl = {75}>
+            <SimpleGrid columns={{sm: 1, md: 4}} spacing={7} width={'100%'} p={70}>
                 {Object.keys(lastItem).map((key) => (
-                    <Stat as={GridItem} key={key} border={'1px'} borderRadius={'10px'} shadow={1}>
+                    <Stat as={GridItem} key={key} border={'1px'} borderRadius={'10px'} shadow={1} boxShadow={'lg'}>
                         <Box>
                             <StatLabel as="h1" size="md" textAlign={'left'} ml={5}>
                                 {keyDisplay[key] || key}
                             </StatLabel>
-                            <StatNumber textAlign={'left'} ml={5}>{lastItem[key]}</StatNumber>
+                            <StatNumber textAlign={'left'} ml={5}>{lastItem[key]} {unitMap[key]}</StatNumber>
                             <StatHelpText textAlign={'left'} ml={5}>
                                 <StatArrow type={lastItem[key] > previousData[key] ? "increase" : "decrease"} />
                                 {getPercentageChange(lastItem[key], previousData[key]).toFixed(2)}%
