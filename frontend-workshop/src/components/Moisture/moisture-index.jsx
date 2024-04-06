bde1679c4a43ebc29f9f52f64423eee82ea2dc73
aeron-cap <aeroncaponpon.01@gmail.com>
2024-04-06 18:19:35 +0800

    try-reload-moisture

--- frontend-workshop/src/components/Moisture/moisture-index.jsx
+++ frontend-workshop/src/components/Moisture/moisture-index.jsx
@@ -5,36 +5,47 @@ import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
 import MoistureTable from './moisture-data';
 
 const moistureIndex = () => {
-  const location = useLocation();
+  //const location = useLocation();
+
+  const intervalTime = 5000;
+
+  // Function to refresh the page
+  function refreshPage() {
+    location.reload(); // Reload the current page
+  }
+
+  // Set interval to call refreshPage function every intervalTime
 
   useEffect(() => {
-    // Function to reset the inactivity timer
-    function resetInactivityTimeout() {
-      clearTimeout(window.inactivityTimeout);
-      window.inactivityTimeout = setTimeout(() => {
-        // Send a request to the server to reset the session timeout
-        fetch('http://localhost:5000/moistureData').then((response) => {
-          if (response.ok) {
-            console.log('Reload');
-            //console.clear();
+    // // Function to reset the inactivity timer
+    // function resetInactivityTimeout() {
+    //   clearTimeout(window.inactivityTimeout);
+    //   window.inactivityTimeout = setTimeout(() => {
+    // Send a request to the server to reset the session timeout
+    fetch('http://localhost:5000/moistureData').then((response) => {
+      if (response.ok) {
+        console.log('Reload');
+        //console.clear();
 
-            // Redirect to the pH view route after 30 seconds of inactivity
-            window.location.href = '/moisture';
-          }
-        });
-      }, 30000); // 30000 = 30 seconds
-    }
+        // Redirect to the pH view route after 30 seconds of inactivity
+        // window.location.href = '/moisture';
+        setInterval(refreshPage, intervalTime);
+      }
+    });
+    //   }, 30000); // 30000 = 30 seconds
+    // }
 
-    // Check if the current page is the pHLevel page before resetting the timeout
-    if (location.pathname === '/moisture') {
-      resetInactivityTimeout();
-    }
+    //   // Check if the current page is the pHLevel page before resetting the timeout
+    //   if (location.pathname === '/moisture') {
+    //     resetInactivityTimeout();
+    //   }
 
-    // Clean up function to clear the timeout when component unmounts or location changes
-    return () => {
-      clearTimeout(window.inactivityTimeout);
-    };
-  }, [location.pathname]);
+    //   // Clean up function to clear the timeout when component unmounts or location changes
+    //   return () => {
+    //     clearTimeout(window.inactivityTimeout);
+    //   };
+  }, []);
+
   return (
     <Tabs isFitted variant="soft-rounded" colorScheme="green">
       <TabList>
