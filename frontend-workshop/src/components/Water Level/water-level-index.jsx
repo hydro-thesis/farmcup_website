fd96401ab38bb7acb1396a5fa0633eacdea55a46
leandrogrng <leandro.garing@gmail.com>
2024-04-08 23:52:23 +0800

    water-level-parameter-page

---
+++ frontend-workshop/src/components/Water Level/water-level-index.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button,
+  Divider,
+  Flex,
+  Grid,
+  Input,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  VStack
+} from '@chakra-ui/react';
+
+import 'chart.js/auto';
+import { Line } from 'react-chartjs-2';
+import axios from 'axios';
+
+function WaterLevelTable() {
+  const [waterLevel, setWaterLevel] = useState([]);
+
+  useEffect(() => {
+    const fetchWaterLevel = async () => {
+      try {
+        const res = await axios.get('http://localhost:5000/waterLevelData');
+        setWaterLevel(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchWaterLevel();
+  }, []);
+
+  // Convert fetched data to chart format
+  const chartData = {
+    labels: waterLevel.map((data) => data.time_stamp),
+    datasets: [
+      {
+        label: 'Water Level Value',
+        data: waterLevel.map((data) => data.water_level),
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }
+    ]
+  };
+  return (
+    <Flex justify="center">
+      <Flex justify="center" minW="25vw">
+        <TableContainer>
+          <Table size="sm" minW="25vw" textAlign="center">
+            <Thead>
+              <Tr>
+                <Th>Water Level</Th>
+                <Th>Time Stamp</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {waterLevel && waterLevel.length > 0 ? (
+                waterLevel.map((data) => (
+                  <Tr key={data.id}>
+                    <Td>{data.water_level}</Td>
+                    <Td>{data.time_stamp}</Td>
+                  </Tr>
+                ))
+              ) : (
+                <Tr>
+                  <Td colSpan={2}>No data available</Td>
+                </Tr>
+              )}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Flex>
+      <Divider orientation="vertical" w="2vw" />
+      <Grid justify="center" minW="50vw">
+        <VStack>
+          <Line data={chartData} />
+          <Divider />
+          <Input variant="outline" placeholder="Enter desired water level here" />
+          <Button>Update</Button>
+        </VStack>
+      </Grid>
+    </Flex>
+  );
+}
+
+WaterLevelTable.propTypes = {};
+
+export default WaterLevelTable;
