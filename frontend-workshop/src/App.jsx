a34676ca76218c0e6b986a1cb14378b579319e86
aeron-cap <aeroncaponpon.01@gmail.com>
2024-04-08 22:21:25 +0800

    copied-navbar-tocurrent-branch

--- frontend-workshop/src/App.jsx
+++ frontend-workshop/src/App.jsx
@@ -4,20 +4,17 @@ import { Link, Outlet, useLocation } from 'react-router-dom';
 import theme from './theme';
 import SideNav from './components/SideNav';
 
-const border = '0px';
-
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Grid
         w="full"
         templateAreas={{ base: `"sidenav" "content"`, md: `"sidenav content"` }}
-        gridTemplateColumns={[`1fr`, `1fr`, `200px 1fr`]}
-        spacing={0}>
-        <GridItem area="sidenav" bgColor={''} border={border}>
+        gridTemplateColumns={[`1fr`, `1fr`, `200px 1fr`]}>
+        <GridItem area="sidenav" bgColor={''}>
           <SideNav />
         </GridItem>
-        <GridItem area="content" bgColor={'RGBA(0, 0, 0, 0.02)'} border={border} ml={7} >
+        <GridItem area="content" bgColor={''}>
           <Outlet />
         </GridItem>
       </Grid>
--- frontend-workshop/src/components/SideNav/index.jsx
+++ frontend-workshop/src/components/SideNav/index.jsx
@@ -1,27 +1,51 @@
 import React, { Fragment } from 'react';
-import { Box, Container, Flex, Stack } from '@chakra-ui/react';
+import { Box, Container, Flex, Stack, VStack, Divider, Text } from '@chakra-ui/react';
 import { Link, useLocation } from 'react-router-dom';
 import routes from './routes';
 
 const SideNav = () => {
-    const { pathname } = useLocation();
-    return (
-        <Container border = {''} display='flex' mb={5}>
-            <Stack border = {''} 
-            height = {{base: '10%', md: '100vh'}} 
-            flexDirection={{base: 'row', md: 'column'}} 
-            justifyContent={{base: `center`, md: `flex-start`}}>
-                {routes.map(({ path, name }) => (
-                    <Box key={`navigation-${name}`}>
-                        <Link key={path} to={path}
-                            className={path === '/' ? (pathname === path ? 'menuActive' : 'menu') : 
-                            (pathname.startsWith(path) ? 'menuActive' : 'menu')}>
-                            {name}
-                        </Link>
-                    </Box>
-                ))}
-            </Stack>
-        </Container>
-    );
+  const { pathname } = useLocation();
+  return (
+    <Container display="flex" bg="gray.200">
+      <Stack
+        marginLeft={3}
+        spacing={2}
+        height={{ base: '10%', md: '100vh' }}
+        flexDirection={{ base: 'row', md: 'column' }}
+        justifyContent={{ base: `center`, md: `flex-start` }}>
+        <VStack>
+          <Box
+            p="60px"
+            color="white"
+            mt="4"
+            bg="teal.500"
+            rounded="md"
+            shadow="md"
+            marginTop={5}></Box>
+          <Divider height="30px" />
+        </VStack>
+        {routes.map(({ path, name }) => (
+          <Box key={`navigation-${name}`} textAlign="left">
+            <Text
+              as={Link}
+              fontSize="sm"
+              key={path}
+              to={path}
+              className={
+                path === '/'
+                  ? pathname === path
+                    ? 'menuActive'
+                    : 'menu'
+                  : pathname.startsWith(path)
+                  ? 'menuActive'
+                  : 'menu'
+              }>
+              {name}
+            </Text>
+          </Box>
+        ))}
+      </Stack>
+    </Container>
+  );
 };
 export default SideNav;
